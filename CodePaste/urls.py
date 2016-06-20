10fe12c4149f0fbfea183911970c148bdd62636b
ashraful_robin <ashrafulrobin3@gmail.com>
2016-06-20 20:08:17 +0600

    Done a Dynamic url routing

--- CodePaste/urls.py
+++ CodePaste/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^$', include('paste.urls')),
-    url(r'^$', 'paste.views.home', name='home'),
+    url(r'^', include('paste.urls')),
+    # url(r'^$', 'paste.views.home', name='home'),
     url(r'^admin/', admin.site.urls),
 ]
