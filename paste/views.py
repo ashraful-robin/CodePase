6c38e1c52a220948169aa759fc6575833bee5d3f
ashraful_robin <ashrafulrobin3@gmail.com>
2016-06-20 20:01:46 +0600

    Done a basic view of code without style

--- paste/views.py
+++ paste/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Paste
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
+
+def home(request):
+    code = Paste.objects.last()
+    lexer = get_lexer_by_name(code.language, stripall=True)
+    formatter = HtmlFormatter(linenos=True, cssclass=code.style)
+    result = highlight(code.new_paste, lexer, formatter)
+    return render(request, 'home.html', {"code": code, "result": result})
